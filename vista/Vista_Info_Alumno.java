d28745f7ae5cb95f2305cc4106e7a700447ea817
Ionutalin23 <ionutalin64@gmail.com>
2020-05-13 11:18:55 +0200

    Añadido funcionalidad Limpiar campos al añadir un nuevo alumno

--- vista/Vista_Info_Alumno.java
+++ vista/Vista_Info_Alumno.java
@@ -31,136 +31,148 @@ public class Vista_Info_Alumno extends JFrame {
 	private JLabel lblUser;
 	private JTextField txtExpediente;
 	private JLabel lblResul;
-	
+	private JPanel pnlContenido;
+
 	public Vista_Info_Alumno() {
-		
-		ImageIcon ico =new ImageIcon(getClass().getResource("/images/logo.png"));
-		ImageIcon ico2 =new ImageIcon(getClass().getResource("/images/uni2.jpg"));
-		
-		Image button1= new ImageIcon(getClass().getResource("/images/boton1.png")).getImage().getScaledInstance(110, 48, Image.SCALE_SMOOTH);
-		Image button2= new ImageIcon(getClass().getResource("/images/boton2.png")).getImage().getScaledInstance(110, 48, Image.SCALE_SMOOTH);
-		Image button3= new ImageIcon(getClass().getResource("/images/boton3.png")).getImage().getScaledInstance(110, 48, Image.SCALE_SMOOTH);
-		
-		Image back1= new ImageIcon(getClass().getResource("/images/back1.png")).getImage().getScaledInstance(24, 24, Image.SCALE_SMOOTH);
-		Image back2= new ImageIcon(getClass().getResource("/images/back2.png")).getImage().getScaledInstance(24, 24, Image.SCALE_SMOOTH);
-		Image back3= new ImageIcon(getClass().getResource("/images/back3.png")).getImage().getScaledInstance(24, 24, Image.SCALE_SMOOTH);
-		
+
+		ImageIcon ico = new ImageIcon(getClass().getResource("/images/logo.png"));
+		ImageIcon ico2 = new ImageIcon(getClass().getResource("/images/uni2.jpg"));
+
+		Image button1 = new ImageIcon(getClass().getResource("/images/boton1.png")).getImage().getScaledInstance(110,
+				48, Image.SCALE_SMOOTH);
+		Image button2 = new ImageIcon(getClass().getResource("/images/boton2.png")).getImage().getScaledInstance(110,
+				48, Image.SCALE_SMOOTH);
+		Image button3 = new ImageIcon(getClass().getResource("/images/boton3.png")).getImage().getScaledInstance(110,
+				48, Image.SCALE_SMOOTH);
+
+		Image back1 = new ImageIcon(getClass().getResource("/images/back1.png")).getImage().getScaledInstance(24, 24,
+				Image.SCALE_SMOOTH);
+		Image back2 = new ImageIcon(getClass().getResource("/images/back2.png")).getImage().getScaledInstance(24, 24,
+				Image.SCALE_SMOOTH);
+		Image back3 = new ImageIcon(getClass().getResource("/images/back3.png")).getImage().getScaledInstance(24, 24,
+				Image.SCALE_SMOOTH);
+
 		setTitle("");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 888, 664);
 		getContentPane().setLayout(null);
 //		POSICIONAR VENTANA EN EL CENTRO DE LA PANTALLA
 		setLocationRelativeTo(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 99, 71));
 		panel.setBounds(0, 0, 870, 617);
 		getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		JPanel pnlUser = new JPanel();
 		pnlUser.setForeground(new Color(240, 248, 255));
-		pnlUser.setBackground(new Color(192, 192, 192,190));
+		pnlUser.setBackground(new Color(192, 192, 192, 190));
 		pnlUser.setBounds(0, 0, 208, 27);
 		panel.add(pnlUser);
 		pnlUser.setLayout(null);
-		
+
 		lblUser = new JLabel("Logged as: Pedro Camacho");
 		lblUser.setForeground(new Color(255, 255, 255));
 		lblUser.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblUser.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUser.setBounds(0, 0, 208, 27);
 		pnlUser.add(lblUser);
-		
+
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setForeground(new Color(0, 0, 0));
 		lblLogo.setBounds(25, 555, 162, 49);
 		panel.add(lblLogo);
-		ImageIcon img=new ImageIcon(ico.getImage().getScaledInstance(lblLogo.getWidth(), lblLogo.getHeight(), Image.SCALE_SMOOTH));
+		ImageIcon img = new ImageIcon(
+				ico.getImage().getScaledInstance(lblLogo.getWidth(), lblLogo.getHeight(), Image.SCALE_SMOOTH));
 		lblLogo.setIcon(img);
 		lblLogo.setBackground(new Color(0, 0, 0));
-		
-		JPanel pnlContenido = new JPanel();
+
+		pnlContenido = new JPanel();
 		pnlContenido.setBounds(230, 32, 405, 561);
 		panel.add(pnlContenido);
-		pnlContenido.setBackground(new Color(226,106,106,240));
+		pnlContenido.setBackground(new Color(226, 106, 106, 240));
 		pnlContenido.setLayout(null);
-		
+
 		JLabel lblTitle = new JLabel("Informaci\u00F3n del alumno:");
 		lblTitle.setForeground(new Color(255, 255, 255));
 		lblTitle.setFont(new Font("Century Gothic", Font.BOLD, 24));
 		lblTitle.setBounds(32, 53, 296, 29);
 		pnlContenido.add(lblTitle);
-		
+
 		JLabel lblDni = new JLabel("DNI:");
 		lblDni.setForeground(new Color(0, 0, 0));
 		lblDni.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblDni.setBounds(32, 95, 56, 16);
 		pnlContenido.add(lblDni);
-		
+
 		txtDni = new JTextField();
 		txtDni.setBounds(32, 124, 267, 22);
 		pnlContenido.add(txtDni);
 		txtDni.setColumns(10);
-		
+
 		JLabel lblName = new JLabel("NOMBRE:");
 		lblName.setForeground(Color.BLACK);
 		lblName.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblName.setBounds(32, 159, 77, 16);
 		pnlContenido.add(lblName);
-		
+
 		txtName = new JTextField();
 		txtName.setColumns(10);
 		txtName.setBounds(32, 188, 267, 22);
 		pnlContenido.add(txtName);
-		
+
 		JLabel lblApellidos = new JLabel("APELLIDOS:");
 		lblApellidos.setForeground(Color.BLACK);
 		lblApellidos.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblApellidos.setBounds(32, 223, 92, 16);
 		pnlContenido.add(lblApellidos);
-		
+
 		txtApellidos = new JTextField();
 		txtApellidos.setColumns(10);
 		txtApellidos.setBounds(32, 252, 267, 22);
 		pnlContenido.add(txtApellidos);
-		
+
 		JLabel lblNacim = new JLabel("FECHA DE NACIMIENTO:");
 		lblNacim.setForeground(Color.BLACK);
 		lblNacim.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblNacim.setBounds(32, 351, 177, 16);
 		pnlContenido.add(lblNacim);
-		
+
 		txtNacim = new JTextField();
 		txtNacim.setColumns(10);
 		txtNacim.setBounds(32, 380, 267, 22);
 		pnlContenido.add(txtNacim);
-		
+
 		lblLogout = new JLabel("LOGOUT");
 		lblLogout.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLogout.setForeground(Color.WHITE);
 		lblLogout.setBackground(new Color(205, 92, 92));
 		lblLogout.setBounds(301, 13, 92, 27);
 		pnlContenido.add(lblLogout);
-		
+
 		JLabel lblLogoutButton = new JLabel("");
 		lblLogoutButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				lblLogoutButton.setIcon(new ImageIcon(button3));
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				lblLogoutButton.setIcon(new ImageIcon(button2));
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				lblLogoutButton.setIcon(new ImageIcon(button2));
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				lblLogoutButton.setIcon(new ImageIcon(button1));
 			}
+
 			public void mouseClicked(MouseEvent e) {
 				miControlador.logout6();
 			}
@@ -168,25 +180,29 @@ public class Vista_Info_Alumno extends JFrame {
 		lblLogoutButton.setBounds(301, 13, 92, 27);
 		pnlContenido.add(lblLogoutButton);
 		lblLogoutButton.setIcon(new ImageIcon(button1));
-		
+
 		lblBack = new JLabel("");
 		lblBack.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				lblBack.setIcon(new ImageIcon(back3));
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				lblBack.setIcon(new ImageIcon(back2));
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				lblBack.setIcon(new ImageIcon(back2));
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				lblBack.setIcon(new ImageIcon(back1));
 			}
+
 			public void mouseClicked(MouseEvent e) {
 				miControlador.back5();
 			}
@@ -194,43 +210,47 @@ public class Vista_Info_Alumno extends JFrame {
 		lblBack.setIcon(new ImageIcon(Vista_Info_Alumno.class.getResource("/images/back1.png")));
 		lblBack.setBounds(35, 13, 24, 24);
 		pnlContenido.add(lblBack);
-		
+
 		JLabel lblNacionalidad = new JLabel("NACIONALIDAD:");
 		lblNacionalidad.setForeground(Color.BLACK);
 		lblNacionalidad.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblNacionalidad.setBounds(32, 415, 131, 16);
 		pnlContenido.add(lblNacionalidad);
-		
+
 		txtNacionalidad = new JTextField();
 		txtNacionalidad.setColumns(10);
 		txtNacionalidad.setBounds(32, 444, 267, 22);
 		pnlContenido.add(txtNacionalidad);
-		
+
 		JLabel lblCrear = new JLabel("CREAR");
 		lblCrear.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCrear.setForeground(Color.WHITE);
 		lblCrear.setBackground(new Color(205, 92, 92));
 		lblCrear.setBounds(134, 500, 109, 48);
 		pnlContenido.add(lblCrear);
-		
+
 		JLabel lblCrearButton = new JLabel("");
 		lblCrearButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				lblCrearButton.setIcon(new ImageIcon(button3));
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				lblCrearButton.setIcon(new ImageIcon(button2));
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				lblCrearButton.setIcon(new ImageIcon(button2));
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				lblCrearButton.setIcon(new ImageIcon(button1));
 			}
+
 			public void mouseClicked(MouseEvent e) {
 				miControlador.insertarDato();
 			}
@@ -238,34 +258,34 @@ public class Vista_Info_Alumno extends JFrame {
 		lblCrearButton.setBounds(133, 500, 110, 48);
 		pnlContenido.add(lblCrearButton);
 		lblCrearButton.setIcon(new ImageIcon(button1));
-		
+
 		JLabel lblExpediente = new JLabel("NUMERO EXPEDIENTE:");
 		lblExpediente.setForeground(Color.BLACK);
 		lblExpediente.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblExpediente.setBounds(32, 287, 177, 16);
 		pnlContenido.add(lblExpediente);
-		
+
 		txtExpediente = new JTextField();
 		txtExpediente.setColumns(10);
 		txtExpediente.setBounds(32, 316, 267, 22);
 		pnlContenido.add(txtExpediente);
-		
+
 		lblResul = new JLabel("");
 		lblResul.setForeground(new Color(128, 0, 0));
 		lblResul.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblResul.setHorizontalAlignment(SwingConstants.CENTER);
 		lblResul.setBounds(32, 479, 267, 16);
 		pnlContenido.add(lblResul);
-		
+
 		JLabel lblPortada = new JLabel("");
 		lblPortada.setHorizontalAlignment(SwingConstants.LEFT);
 		lblPortada.setBounds(0, 0, 870, 617);
 		panel.add(lblPortada);
-		ImageIcon img2=new ImageIcon(ico2.getImage().getScaledInstance(lblPortada.getWidth(), lblPortada.getHeight(), Image.SCALE_SMOOTH));
+		ImageIcon img2 = new ImageIcon(
+				ico2.getImage().getScaledInstance(lblPortada.getWidth(), lblPortada.getHeight(), Image.SCALE_SMOOTH));
 		lblPortada.setIcon(new ImageIcon(Vista_Info_Alumno.class.getResource("/images/resi.jpg")));
 		lblPortada.setBackground(new Color(0, 0, 0));
-		
-	
+
 	}
 
 	public void setControlador(controlador miControlador) {
@@ -277,7 +297,7 @@ public class Vista_Info_Alumno extends JFrame {
 	}
 
 	public void actualizarLogged() {
-		lblUser.setText("Logged as: "+miModelo.getUSR());
+		lblUser.setText("Logged as: " + miModelo.getUSR());
 	}
 
 	public String getTxtDni() {
@@ -305,14 +325,23 @@ public class Vista_Info_Alumno extends JFrame {
 	}
 
 	public void actualizar() {
-		String resultado= miModelo.getResultadoAlum();
+		String resultado = miModelo.getResultadoAlum();
 		if (resultado.equals("EXISTENTE")) {
 			lblResul.setText("EL USUARIO YA EXISTE");
-		}else if(resultado.equals("EXITO")) {
+		} else if (resultado.equals("EXITO")) {
 			lblResul.setText("USUARIO AÑADIDO CON EXITO!!");
-		}else {
+			clearFields();
+		} else {
 			lblResul.setText("ERROR");
 		}
 	}
-	
+
+	public void clearFields() {
+		for (Object obj : pnlContenido.getComponents()) {
+			if (obj instanceof JTextField) {
+				((JTextField) obj).setText("");
+			}
+		}
+	}
+
 }
