4eb38b0d86e07984fb45dd81b15ecfa41bcdb505
Fabiola <Fabiola@DESKTOP-9ALVRN1>
2020-05-29 23:03:34 +0200

    Modificar Tutor

--- modelo/modelo.java
+++ modelo/modelo.java
@@ -968,14 +968,14 @@ public class modelo {
 		
 	}
 	
-	public void modificarTutor(String dni, String nombre, String apellidos, String codCentro) {
+	public void modificarTutor(String dni, String nombre, String apellidos, int codCentro) {
 		PreparedStatement ps;
 		try {		
 			ps = conexion.prepareStatement("UPDATE PI.tutor SET dni_tutor=?, nombre = ?, apellidos = ?, centro_cod_centro = ?  WHERE dni_tutor = ?");
 			ps.setString(1, dni);
 			ps.setString(2, nombre);
 			ps.setString(3, apellidos);
-			ps.setString(4, codCentro);
+			ps.setInt(4, codCentro);
 			
 			int resul = ps.executeUpdate();
 		} catch (SQLException e) {
